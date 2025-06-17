8ec0f8800847af3ad3c7062f685e41bf3a4d0cbd
gustavogordoni <gustavogordoni1@gmail.com>
2025-06-17 15:32:07 -0300

    docs: URL annotations assets items

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -22,56 +22,56 @@ class ItemFactory extends Factory
         return [
             'name' => fake()->words(2, true),
             'rarity' => fake()->randomElement([
-                'Unobtainable',
-                'Contraband',
-                'Relic',
-                'Legendary',
-                'Epic',
-                'Rare',
-                'Uncommon'
+                'Unobtainable', // rarity: 6
+                'Contraband', // rarity: 5
+                'Relic', // rarity: 4
+                'Legendary', // rarity: 3
+                'Epic', // rarity: 2
+                'Rare', // rarity: 1
+                'Uncommon' // rarity: 0
             ]),
             'season' => fake()->numberBetween(1, 8),
             'category' => fake()->randomElement([
-                'Sniper Rifle', // 1
-                'Assault Rifle', // 2
-                'Pistol', // 
-                'Submachine Gun', // 4
-                'Revolver', // 5
-                'Shotgun', // 6
-                'Machine Gun', // 7
-                'Semi Auto', // 8
-                'Rocket Launcher', // 9
-                'Akimbo Uzi', // 10
-                'Desert Eagle', // 11
-                'Alien Blaster', // 12
-                'Crossbow', // 14
-                'Famas', // 15
-                'Sawed OFF', // 16
-                'Auto Pistol', // 17
-                'Blaster', // 19
-                'Grappler', // 
-                'Tehchy', // 22
-                'Noob Tube', // 23
-                'Zapper', // 25
-                'Akimbo Pistol', // 28
-                'Charge Rifle', // 19
+                'Sniper Rifle', // weapons/1_x.png
+                'Assault Rifle', // weapons/2_x.png
+                'Pistol', // weapons/3_x.png
+                'Submachine Gun', // weapons/4_x.png
+                'Revolver', // weapons/5_x.png
+                'Shotgun', // weapons/6_x.png
+                'Machine Gun', // weapons/7_x.png
+                'Semi Auto', // weapons/8_x.png
+                'Rocket Launcher', // weapons/9_x.png
+                'Akimbo Uzi', // weapons/10_x.png
+                'Desert Eagle', // weapons/11_x.png
+                'Alien Blaster', // weapons/12_x.png
+                'Crossbow', // weapons/14_x.png
+                'Famas', // weapons/15_x.png
+                'Sawed OFF', // weapons/16_x.png
+                'Auto Pistol', // weapons/17_x.png
+                'Blaster', // weapons/19_x.png
+                'Grappler', // weapons/_x.png
+                'Tehchy', // weapons/22_x.png
+                'Noob Tube', // weapons/23_x.png
+                'Zapper', // weapons/25_x.png
+                'Akimbo Pistol', // weapons/28_x.png
+                'Charge Rifle', // weapons/19_x.png
                 'Compressor',
-                'Hats',
+                'Hats', // cosmetics/1_x.png
+                'Back', // cosmetics/2_x.png
                 'Body',
-                'Melee',
-                'Sprays',
-                'Dyes',
-                'Waist',
-                'Faces',
-                'Shoes',
-                'Pets',
-                'Collectibles',
-                'Wrist',
-                'Charms',
-                'Tickets',
-                'Back',
+                'Melee', // melee/melee_x.png
+                'Sprays', // sprays/459.png (sem "previews/" antes)
+                'Dyes', // cosmetics/5_x.png
+                'Waist', // cosmetics/6_x.png
+                'Faces', // cosmetics/7_x.png
+                'Shoes', // cosmetics/8_x.png
+                'Pets', // cosmetics/9_x.png
+                'Collectibles', // cosmetics/10_x.png
+                'Wrist', // cosmetics/11_x.png
+                'Charms', // cosmetics/12_x.png
+                'Tickets', // cosmetics/13_0_x.png
                 'Head',
-                'Playercards'
+                'Playercards' /// playercards/card_x.webp (sem "previews/" antes)
             ]),
             'tag' => fake()->optional()->randomElement(['Vaulted', 'Twitch', 'Raid', '???', 'Kanji', 'Subzero', 'Christmas']),
             'author' => fake()->numberBetween(1, 10),
@@ -82,6 +82,11 @@ class ItemFactory extends Factory
     private function getValidImageUrl(): ?string
     {
         $defaultUrl = 'https://assets.krunker.io/textures/previews/weapons/weapon_1_1.png';
+        // https://assets.krunker.io/textures/previews/cosmetics/6_79.png
+        // https://assets.krunker.io/textures/previews/melee/melee_115.png
+        // https://assets.krunker.io/textures/previews/cosmetics/13_0_8.png
+        // https://assets.krunker.io/textures/sprays/459.png
+        // https://assets.krunker.io/textures/playercards/card_9.webp
 
         try {
             $check = Http::head($defaultUrl);
